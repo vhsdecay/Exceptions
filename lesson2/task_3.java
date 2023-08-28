0252b1edd34b19f0395619146923b5dc55d0f510
vhsdecay <vetvwhs@gmail.com>
2023-08-28 16:26:29 +0300

    Урок 2 Задача 3

---
+++ lesson2/task_3.java
@@ -0,0 +1,26 @@
+package lesson2;
+
+import java.io.FileNotFoundException;
+
+public class task_3 {
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = {1, 2, 2, 6};
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+
+    }
+    public static void printSum(Integer a, Integer b){
+            System.out.println(a + b);
+    }
+}
