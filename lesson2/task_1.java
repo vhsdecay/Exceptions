166a68f05567d880d44b831104c0ad41b7426948
vhsdecay <vetvwhs@gmail.com>
2023-08-28 16:09:50 +0300

    Урок 2 Задача 1

--- lesson2/task_1.java
+++ lesson2/task_1.java
@@ -1,6 +1,24 @@
 package lesson2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class task_1 {
+    public static void main(String[] args) {
+        boolean work = true;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        while(work){
+            System.out.print("Введите дробное число: ");
+            try{
+                float fNumber = Float.parseFloat(reader.readLine());
+                System.out.printf("Введенное число равно %.3f\n", fNumber);
+                work = false;
+            } catch (IOException|NumberFormatException e) {
+                System.out.println("Неверный ввод. Введите дробное число!!!");
+            }
 
+        }
+    }
 }
